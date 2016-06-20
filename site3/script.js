008f0a62f2991fdbe8ed17d7d644a1a3cde37dc4
Fatumo Oluwatobi Ifeoluwa <tobyfatumo@gmail.com>
2016-06-20 16:22:56 +0100

    Update script.js

--- site3/script.js
+++ site3/script.js
@@ -1,4 +1,4 @@
-(function (window) {
+(function (j) {
 // *******************************
 // START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
@@ -57,9 +57,9 @@ for (var i in names) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter == 'j') {
-     window.byeSpeaker.speak(names[i]);
+     j.byeSpeaker.speak(names[i]);
   } else {
-     window.helloSpeaker.speak(names[i]);
+     j.helloSpeaker.speak(names[i]);
   }
 }
-})(window);
+})(j);
